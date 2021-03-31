bf483642be6e5f8711abf90573b89c33f20da4aa
Bhole Nath Maddheshiya <59330174+BholeNath3456@users.noreply.github.com>
2021-03-31 08:08:52 +0530

    In DeliveryActivity set new Addresses

--- OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/MyAddressesActivity.java
+++ OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/MyAddressesActivity.java
@@ -7,10 +7,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SimpleItemAnimator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +24,10 @@ import static com.example.onlinemoneypay.DeliveryActivity.SELECT_ADDRESS;
 public class MyAddressesActivity extends AppCompatActivity {
     private RecyclerView myAddressesRecyclerView;
     private Button deliverHereBtn;
+    private LinearLayout addNewAddressBtn;
     private static AddressesAdapter addressesAdapter;
+    private TextView addressSaved;
+    private int previousAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +39,20 @@ public class MyAddressesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         myAddressesRecyclerView = findViewById(R.id.addresses_recyclerview);
         deliverHereBtn=findViewById(R.id.deliver_here_btn);
+        addNewAddressBtn=findViewById(R.id.add_new_address_btn);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         myAddressesRecyclerView.setLayoutManager(layoutManager);
+        addressSaved=findViewById(R.id.address_saved);
+        previousAddress=DBqueries.selectedAddress;
+//        List<AddressesModel> addressesModelList = new ArrayList<>();
+//        addressesModelList.add(new AddressesModel("Bhole Nath", "Chappwa, Nautanwa", "273417",true));
+//        addressesModelList.add(new AddressesModel("Niyanta Karki", "Chappwa, Nautanwa", "273417",false));
+//        addressesModelList.add(new AddressesModel("Shivangini", "Chappwa, Nautanwa", "273417",false));
+//        addressesModelList.add(new AddressesModel("Anjali Chaudhary", "Chappwa, Nautanwa", "273417",false));
+//
 
-        List<AddressesModel> addressesModelList = new ArrayList<>();
-        addressesModelList.add(new AddressesModel("Bhole Nath", "Chappwa, Nautanwa", "273417",true));
-        addressesModelList.add(new AddressesModel("Niyanta Karki", "Chappwa, Nautanwa", "273417",false));
-        addressesModelList.add(new AddressesModel("Shivangini", "Chappwa, Nautanwa", "273417",false));
-        addressesModelList.add(new AddressesModel("Anjali Chaudhary", "Chappwa, Nautanwa", "273417",false));
+
         int mode=getIntent().getIntExtra("MODE",-1);
         if(mode==SELECT_ADDRESS){
             deliverHereBtn.setVisibility(View.VISIBLE);
@@ -49,10 +61,24 @@ public class MyAddressesActivity extends AppCompatActivity {
             deliverHereBtn.setVisibility(View.GONE);
 
         }
-         addressesAdapter = new AddressesAdapter(addressesModelList,mode);
+        deliverHereBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyAddressesActivity.this, "continue To Deliver", Toast.LENGTH_SHORT).show();
+            }
+        });
+        addressesAdapter = new AddressesAdapter(DBqueries.addressesModelList,mode);
         myAddressesRecyclerView.setAdapter(addressesAdapter);
         ((SimpleItemAnimator)myAddressesRecyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
         addressesAdapter.notifyDataSetChanged();
+        addNewAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addAddressIntent=new Intent(MyAddressesActivity.this,AddAddressActivity.class);
+                startActivity(addAddressIntent);
+            }
+        });
+        addressSaved.setText(String.valueOf(DBqueries.addressesModelList.size()));
     }
 
     public static void refreshItem(int deselect,int select){
