8f62f35a7cfdf59b1085baf34e94b51456b74fbf
Bhole Nath Maddheshiya <59330174+BholeNath3456@users.noreply.github.com>
2021-04-02 08:20:38 +0530

    cash back applied from user.

--- OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/ProductDetailsActivity.java
+++ OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/ProductDetailsActivity.java
@@ -469,11 +469,21 @@ public class ProductDetailsActivity extends AppCompatActivity {
         applyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: "+selectedRewardId);
-                Toast.makeText(ProductDetailsActivity.this, selectedRewardId, Toast.LENGTH_SHORT).show();
-//                DBqueries.rewardModelList.contains()
-//                FirebaseFirestore.getInstance().collection("USERS").document(FirebaseAuth.getInstance().getUid()).collection("USERS_REWARDS").document("removethis").delete();
-            }
+                FirebaseFirestore.getInstance().collection("USERS").document(FirebaseAuth.getInstance().getUid()).collection("USERS_REWARDS").document(selectedRewardId)
+                        .update("is_applied",true).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                          if(task.isSuccessful()){
+                              Log.d(TAG, "onClick: "+selectedRewardId);
+                              Toast.makeText(ProductDetailsActivity.this, "Congratulation, coupen has been Applied!", Toast.LENGTH_SHORT).show();
+                          }else {
+                              String error=task.getException().getMessage();
+                              Toast.makeText(ProductDetailsActivity.this, error, Toast.LENGTH_SHORT).show();
+                          }
+                    }
+                });
+
+                }
         });
         /////// coupen  diaglog....
 
