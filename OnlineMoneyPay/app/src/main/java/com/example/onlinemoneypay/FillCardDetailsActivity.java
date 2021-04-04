cae716ac33d118eae642ffbcb228178612d449d5
Bhole Nath Maddheshiya <59330174+BholeNath3456@users.noreply.github.com>
2021-04-04 10:26:02 +0530

    upload Orders Map in Firebase

--- OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/FillCardDetailsActivity.java
+++ OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/FillCardDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.onlinemoneypay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
@@ -11,10 +12,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.type.Date;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class FillCardDetailsActivity extends AppCompatActivity {
@@ -27,6 +38,8 @@ public class FillCardDetailsActivity extends AppCompatActivity {
     private Dialog loadingDialog;
     private ImageView loadingAnim;
     public static List<String> orderProductID;
+    private Timestamp timestamp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +71,41 @@ public class FillCardDetailsActivity extends AppCompatActivity {
         payNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent=new Intent(FillCardDetailsActivity.this,FinishedOrderedActivity.class);
-//                startActivity(intent);
-//                finish();
                 loadingDialog.show();
+                settingOrdersInFirebase();
             }
         });
+
+    }
+
+
+    private void settingOrdersInFirebase() {
+        Map<String, Object> settingOrders = new HashMap<>();
+        settingOrders.put("CustomerID", FirebaseAuth.getInstance().getUid());
+        settingOrders.put("TransactionID", transactionId);
+        settingOrders.put("isPaid", true);
+        settingOrders.put("NumberOfProducts", (MyCartFragment.cartItemModelsList.size() - 1));
+        settingOrders.put("TotalPrice", DeliveryActivity.totalItemPrice );
+        settingOrders.put("OrderedDate", FieldValue.serverTimestamp());
+        settingOrders.put("PackedDate",timestamp);
+        settingOrders.put("ShippingDate",timestamp);
+        settingOrders.put("DeliveredDate",timestamp);
+
+        FirebaseFirestore.getInstance().collection("ORDERS").document(orderId)
+                .set(settingOrders).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    loadingDialog.dismiss();
+                    Intent intent = new Intent(FillCardDetailsActivity.this, FinishedOrderedActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    String error = task.getException().getMessage();
+                    Toast.makeText(FillCardDetailsActivity.this, error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
