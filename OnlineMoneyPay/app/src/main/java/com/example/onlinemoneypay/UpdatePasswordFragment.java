d6f7a1fe85d69e9af6cbaf47e708a9ea88b745da
Bhole Nath Maddheshiya <59330174+BholeNath3456@users.noreply.github.com>
2021-04-06 09:43:37 +0530

    layout fragment update info changes

--- OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/UpdatePasswordFragment.java
+++ OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/UpdatePasswordFragment.java
@@ -2,22 +2,13 @@ package com.example.onlinemoneypay;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,14 +21,6 @@ public class UpdatePasswordFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private EditText oldPassword;
-    private EditText newPassword;
-    private EditText confirmNewPassword;
-    private Button updateBtn;
-    private String inputOldPassword;
-    private String inputNewPassword;
-    private String inputConfirmedPassword;
-    private String email;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -78,63 +61,7 @@ public class UpdatePasswordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_update_password, container, false);
-        oldPassword = view.findViewById(R.id.old_password);
-        newPassword = view.findViewById(R.id.new_password);
-        confirmNewPassword = view.findViewById(R.id.confirm_new_password);
-        updateBtn = view.findViewById(R.id.update_password_btn);
+        return inflater.inflate(R.layout.fragment_update_password, container, false);
 
-        email = getArguments().getString("Email");
-
-        inputOldPassword = oldPassword.getText().toString();
-        inputNewPassword = newPassword.getText().toString();
-        inputConfirmedPassword = confirmNewPassword.getText().toString();
-
-        updateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkInputs();
-            }
-        });
-
-        return view;
-    }
-
-    private void checkInputs() {
-        if (inputNewPassword.equals(inputConfirmedPassword)) {
-            // to do..
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-            AuthCredential credential = EmailAuthProvider.getCredential(email, inputOldPassword);
-            user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if (task.isSuccessful()) {
-                        user.updatePassword(inputNewPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    oldPassword.setText(null);
-                                    newPassword.setText(null);
-                                    confirmNewPassword.setText(null);
-                                    Toast.makeText(getContext(), "Password Updated Successfully!", Toast.LENGTH_SHORT).show();
-
-                                } else {
-                                    String error = task.getException().getMessage();
-                                    Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    } else {
-                        String error = task.getException().getMessage();
-                        Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-        } else {
-            confirmNewPassword.setError("Password does not Matched!");
-            confirmNewPassword.setFocusable(true);
-        }
     }
 }
