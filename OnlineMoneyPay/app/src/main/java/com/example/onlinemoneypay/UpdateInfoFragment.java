7273d101375ca77109e2778126b86273b4719f6c
Bhole Nath Maddheshiya <59330174+BholeNath3456@users.noreply.github.com>
2021-04-06 15:29:14 +0530

    upload image successfully.

--- OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/UpdateInfoFragment.java
+++ OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/UpdateInfoFragment.java
@@ -1,12 +1,39 @@
 package com.example.onlinemoneypay;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +41,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class UpdateInfoFragment extends Fragment {
+    private static final String TAG = "UpdateInfoFragment";
+    private Button selectPhoto, updatePhoto, updateName, updateEmail;
+    private EditText inputName, inputEmail;
+    private CircleImageView userPhoto;
+    private FirebaseUser user;
+    private Uri uri = null;
+    private boolean isUpdatePhoto = false;
+    private StorageReference mStorageReference;
+    private String imgUrl=null;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +95,115 @@ public class UpdateInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_update_info, container, false);
+        userPhoto = view.findViewById(R.id.profile_image);
+        selectPhoto = view.findViewById(R.id.select_photo);
+        updatePhoto = view.findViewById(R.id.update_photo);
+        updateName = view.findViewById(R.id.name_update_btn);
+        updateEmail = view.findViewById(R.id.email_update_btn);
+        inputName = view.findViewById(R.id.user_updated_name);
+        inputEmail = view.findViewById(R.id.user_updated_email);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mStorageReference=FirebaseStorage.getInstance().getReference("UserProfile");
+        selectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (getContext().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                        galleryIntent.setType("image/*");
+                        startActivityForResult(galleryIntent, 1);
+                    } else {
+                        getActivity().requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+                    }
+                } else {
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent, 1);
+                }
+                Toast.makeText(getContext(), "You clicked Me!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        updateName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Update your Name", Toast.LENGTH_SHORT).show();
+            }
+        });
+        updateEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "You can update Email", Toast.LENGTH_SHORT).show();
+            }
+        });
+        updatePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadPhotoInFirebase();
+            }
+        });
+
+        return view;
     }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cr = getContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void uploadPhotoInFirebase() {
+             if(uri!=null){
+                 StorageReference fileReference=mStorageReference.child(user.getUid()+""+getFileExtension(uri));
+                 fileReference.putFile(uri)
+                         .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                             @Override
+                             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                 Log.d(TAG, "onSuccess: "+taskSnapshot.getMetadata());
+                             }
+                         }).addOnFailureListener(new OnFailureListener() {
+                     @Override
+                     public void onFailure(@NonNull Exception e) {
+                         Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                     }
+                 });
+             }else {
+                 Toast.makeText(getContext(), "No File Selected.", Toast.LENGTH_SHORT).show();
+             }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == getActivity().RESULT_OK && data != null && data.getData() != null) {
+                if (data != null) {
+                    uri = data.getData();
+
+                    Log.d(TAG, "onActivityResult: " + uri);
+                    Glide.with(getContext()).load(uri).into(userPhoto);
+
+                } else {
+                    Toast.makeText(getContext(), "Image not found!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, 1);
+            } else {
+                Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
